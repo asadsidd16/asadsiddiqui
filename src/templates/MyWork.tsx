afd1265b7ef887117f0109bc19d94095e6bfd6c0
asadsidd16 <asadsidd22@outlook.com>
2025-05-09 23:03:10 -0700

    my work added

--- src/templates/MyWork.tsx
+++ src/templates/MyWork.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 import { Background } from '../background/Background';
@@ -6,6 +7,7 @@ import { Section } from '../layout/Section';
 const mywork = [
   {
     company: 'neatMon',
+    image: '/neatMonHome.png',
     description:
       'Built and maintained internal tools using Vue and TypeScript.',
     technologies: ['React', 'TypeScript'],
@@ -13,6 +15,7 @@ const mywork = [
   },
   {
     company: 'Rover ERP',
+    image: '/roverHome.png',
     description:
       'Collaborated with design and product teams to revamp user flows.',
     technologies: ['TypeScript', 'Vue', 'Figma'],
@@ -20,13 +23,15 @@ const mywork = [
   },
   {
     company: 'OroCarrot',
+    image: '/orocarrotHome.png',
     description:
       'Collaborated with design and product teams to revamp user flows.',
     technologies: ['Shopify'],
-    link: 'https://orocarrot.com',
+    link: 'https://orocarrottanning.com/',
   },
   {
     company: 'Coastal Viticultural Consultants, Inc.',
+    image: '/cvcHome.png',
     description:
       'Collaborated with design and product teams to revamp user flows.',
     technologies: ['PHP', 'Contentful'],
@@ -34,10 +39,11 @@ const mywork = [
   },
   {
     company: 'DishOut',
+    image: '/dishOutHome.png',
     description:
       'Collaborated with design and product teams to revamp user flows.',
     technologies: ['React', 'JavaScript'],
-    link: 'https://dishout.com',
+    link: 'https://mydishoutapp.com/',
   },
 ];
 
@@ -47,32 +53,43 @@ const MyWork = () => {
       <Section yPadding="py-6">
         <div className="grid grid-cols-1 gap-6 p-4 sm:grid-cols-2 md:grid-cols-3">
           {mywork.map((work) => (
-            <div
-              key={work.company}
-              className="rounded-2xl bg-white p-6 shadow-md transition duration-300 hover:shadow-xl"
-            >
-              <h3 className="mb-2 text-sm font-semibold text-gray-900">
-                {work.company}
-              </h3>
-              <p className="mb-4 text-sm text-gray-700">{work.description}</p>
+            <>
+              <div
+                key={work.company}
+                className="rounded-2xl bg-white p-6 shadow-md transition duration-300 hover:shadow-xl"
+              >
+                <div className="relative mb-4 h-48 w-full">
+                  <Image
+                    src={work.image}
+                    alt="website image"
+                    fill
+                    className="rounded-lg object-cover"
+                  />
+                </div>
 
-              <div className="mb-4 flex flex-wrap gap-2">
-                {work.technologies.map((tech, index) => (
-                  <span
-                    key={index}
-                    className="rounded-full bg-gray-100 px-3 py-1 text-sm font-medium text-gray-800 shadow-sm"
-                  >
-                    {tech}
-                  </span>
-                ))}
+                <h3 className="mb-2 text-sm font-semibold text-gray-900">
+                  {work.company}
+                </h3>
+                <p className="mb-4 text-sm text-gray-700">{work.description}</p>
+
+                <div className="mb-4 flex flex-wrap gap-2">
+                  {work.technologies.map((tech, index) => (
+                    <span
+                      key={index}
+                      className="rounded-full bg-gray-100 px-3 py-1 text-sm font-medium text-gray-800 shadow-sm"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+
+                <a href={work.link} target="_blank" rel="noopener noreferrer">
+                  <button className="w-full rounded-lg bg-black px-6 py-3 text-sm uppercase text-white transition duration-300 hover:bg-gray-800">
+                    Visit
+                  </button>
+                </a>
               </div>
-
-              <a href={work.link} target="_blank" rel="noopener noreferrer">
-                <button className="w-full rounded-lg bg-black px-6 py-3 text-sm uppercase text-white transition duration-300 hover:bg-gray-800">
-                  Visit
-                </button>
-              </a>
-            </div>
+            </>
           ))}
         </div>
       </Section>
