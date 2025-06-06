a1aff4922e9c6627b44742371e01947bb954bb1d
asadsidd16 <asadsidd22@outlook.com>
2025-06-05 20:50:55 -0700

    google change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,18 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-import { AppConfig } from '../utils/AppConfig';
-
-// Need to create a custom _document because i18n support is not compatible with `next export`.
-class MyDocument extends Document {
-  // eslint-disable-next-line class-methods-use-this
-  render() {
-    return (
-      <Html lang={AppConfig.locale}>
-        <Head>
-          {/* Global Site Tag (gtag.js) */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-3RS4VVNRLE"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Global Site Tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-3RS4VVNRLE"
+        />
+        <script
+          id="gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
@@ -24,16 +20,13 @@ class MyDocument extends Document {
                 page_path: window.location.pathname,
               });
             `,
-            }}
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
