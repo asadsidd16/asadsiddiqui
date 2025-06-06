943aeea54916825de298accd83083087084e8e9b
asadsidd16 <asadsidd22@outlook.com>
2025-06-05 20:43:06 -0700

    undefined checks

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -1,6 +1,7 @@
 export const GA_MEASUREMENT_ID = 'G-3RS4VVNRLE';
 
 export const pageview = (url: string) => {
+  if (typeof window.gtag !== 'function') return;
   window.gtag('config', GA_MEASUREMENT_ID, {
     page_path: url,
   });
@@ -17,6 +18,7 @@ export const event = ({
   label: string;
   value: number;
 }) => {
+  if (typeof window.gtag !== 'function') return;
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
